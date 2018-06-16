4f206c65e1af9a3a64673eb388c2069c32745e62
Ahmad Hobby <ahmad.hobby@gmail.com>
2018-06-16 14:52:21 -0400

    assignment done

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -10,13 +10,48 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *billAmount;
+@property (weak, nonatomic) IBOutlet UITextField *tipPercentage;
+@property (weak, nonatomic) IBOutlet UILabel *tipAmount;
+
+
 @end
 
 @implementation ViewController
 
+- (IBAction)calculateTip:(id)sender {
+    
+  // grab textField.text (bill amount). Save it to a var and log it out
+    // set the label to that value
+
+    NSString *amountInput = self.billAmount.text;
+    NSLog(@"%@", amountInput);
+    
+    self.billAmount.text = amountInput;
+    
+    // get the value from tip percent. Save it to a var and log out out
+    // get these two variables and then compute them
+    
+    NSString *tipPercent = self.tipPercentage.text;
+    NSLog(@"%@", tipPercent);
+    
+    float tip = amountInput.floatValue * (tipPercent.floatValue / 100.0);
+   
+    float result = tip + amountInput.floatValue;
+    
+    // final step: set the value to the label
+    
+    self.tipAmount.text = @(result).stringValue;
+    
+    // 
+    
+    
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.tipAmount.text = @"";
 }
 
 
